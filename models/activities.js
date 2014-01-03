88d6ce75046cd1dc49ed4b872f05c762548ddd96
root <cin@cin-H34.(none)>
2014-01-03 22:07:22 +0800

    add 'models/activities.js', 'models/activity_status.js', 'models/user_activities.js'

---
+++ models/activities.js
@@ -0,0 +1,29 @@
+var _ = require('underscore'),
+	chance = new (require('chance'))(),
+	errors = require('../lib/errors'),
+	encrypt = require('../lib/encrypt'),
+	syBookshelf = require('./base'),
+	ActivitySattus, ActivitySattuses;
+
+ActivitySattus = module.exports = syBookshelf.Model.extend({
+	tableName: 'activity_status',
+	fields: ['id', 'name'],
+	initialize: function () {
+		return ActivitySattus.__super__.initialize.apply(this, arguments);
+	},
+
+	saving: function() {
+		return ret = ActivitySattus.__super__.initialize.apply(this, arguments);
+	}
+}, {
+	randomForge: function() {
+		return ActivitySattus.forge({
+			id: chance.integer(),
+			name: chance.name()
+		});
+	}
+});
+
+ActivitySattuses = ActivitySattus.Set = syBookshelf.Collection.extend({
+	model: ActivitySattus
+});

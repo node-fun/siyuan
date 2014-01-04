85d53415bbd1ad757ecf1fe9965cbb7e98192fb1
root <cin@cin-H34.(none)>
2014-01-04 23:08:30 +0800

    update 'models/activities.js' and 'setup/db.sql'

--- models/activities.js
+++ models/activities.js
@@ -9,15 +9,27 @@ Activity = modele.exports = syBookshelf.Model.extend({
 		'ownerid', 'groupid', 'content', 'maxnum', 'createtime',
 		'starttime', 'duration', 'statusid'
 	],
-	omitInJSON: ['ownerid', 'groupid']
+	omitInJSON: ['ownerid', 'groupid'],
 	saving: function() {
 		return Activity.__super__
 			.saving.apply(this, arguments);
 	}
 }, {
 	randomForge: function() {
+		var status = _.random(0, 3),
+			maxnum = _.random(20, 40);
+			duration = _.random(3, 10);
 		return Activity.forge({
-			'ownerid': ''
+			'content': chance.paragraph(),
+			'maxnum': maxnum,
+			'createtime': new Date(),
+			'starttime': chance.date({string: true}),
+			'duration': chance.(),
+			'statusid': status
 		});
 	}
+});
+
+Activities = Activity.Set = syBookshelf.Collection.extend({
+	model: Activity
 });

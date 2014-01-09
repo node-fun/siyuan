ec2dc7bdd9a06957a7deffc934e585605226e757
Cam <349524438@qq.com>
2014-01-09 21:53:17 +0800

    update

--- routes/groups.js
+++ routes/groups.js
@@ -1,14 +1,15 @@
 /**
  * Created by Cam on 14-1-8.
  */
-var Group = require('../models/groups');
+var Group = require('../models/groups'),
+	Groups = Group.Set;
 
 module.exports = function(app){
 	app.get('/api/groups/find', function(req, res){
 		var offset = req.api.offset,
 			limit = req.api.limit,
 			match = req.query;
-		Group.forge().query(function(qb){
+		Groups.forge().query(function(qb){
 			for(var k in match){
 				qb.where(k, match[k]);
 			}

4da4e09096ba6fbbdad764b27743df6a56b1160f
fritx <uxfritz@163.com>
2014-02-19 18:12:21 +0800

    + docs for api common part

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
+/**
+ * Created by fritz on 1/10/14.
+ * @class api通用
+ */
 var errors = require('../lib/errors'),
 	fs = require('fs');
-;
 
 module.exports = function (app) {
 	require('./users')(app);
@@ -18,6 +21,15 @@ module.exports = function (app) {
 	require('./feedbacks')(app);
 	require('./messages')(app);
 
+	/**
+	 * GET /api/
+	 * @method api通用
+	 * @param {Number} [limit] 每页记录数上限 - 默认`10`
+	 * @param {Number} [offset] 起点偏移量 - 默认`0`
+	 * @param {Number} [page] 页数 - 当`offset`指定时无效, 默认`1`
+	 * @param {Number} [fuzzy] 是否采用模糊搜索 - `0`否 `1`是, 默认`0`
+	 * @return {JSON}
+	 */
 	app.use('/api', function (data, req, res, next) {
 		if (data instanceof Error) return next(data);
 		res.send(data);

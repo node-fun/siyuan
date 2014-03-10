f296cc04302af9e60937b4175de08be33e1d1e40
Cam <349524438@qq.com>
2014-03-10 16:57:19 +0800

    把“发消息列表、收消息列表、回复消息、标记为已读 砍掉，因为这些本来是打算按邮件的形式来做的。现在改为对话的方式

--- routes/messages.js
+++ routes/messages.js
@@ -11,73 +11,7 @@ var Message = require('../models/message'),
 	errors = require('../lib/errors');
 
 module.exports = function (app) {
-	/**
-	 * GET /api/messages/receivelist <br/>
-	 * 需登录
-	 * @method 收消息列表
-	 * @param {String} page/limit/order
-	 * @return {JSON}
-	 {
-		"messages": [
-		{
-			"id": 2,
-			"title": "2发给1的一条消息",
-			"body": "这是内容",
-			"isread": 0,
-			"isreplied": 0,
-			"sendtime": 1393056294000,
-			"sender": {
-				"id": 2,
-				"username": "saceza_837",
-				"regtime": 1362543202000,
-				"isonline": 1,
-				"avatar": "\\avatars\\2.jpg?t=1393055835537",
-				"cover": "\\covers\\2.jpg?t=1393055835700",
-				"profile": {
-					"email": "ga@pahipkiw.io",
-	 "name": "Francisco Brewer",
-	 "gender": "f",
-	 "age": 48,
-	 "grade": 1984,
-	 "university": "Depubiafi University",
-	 "major": "Cupirisa",
-	 "summary": "Nophowip dat hivig celala abma meumraj hawbetir set pikag watob benozveh hu ok za.",
-	 "tag": "dipedkoj,cep,awuvir"
-	 },
-	 "isfollowed": 0
-	 }
-	 }
-	 ]
-	 }
-	 */
-	app.get('/api/messages/receivelist', function (req, res, next) {
-		var user = req.user;
-		if (!user) return next(errors(21301));
-		req.query['receiverid'] = user.id;
-		Messages.forge().fetch({ req: req, related: ['sender'] })
-			.then(function (messages) {
-				next({ messages: messages});
-			}).catch(next);
-	});
-
-	/**
-	 * GET /api/messages/sendlist <br/>
-	 * 需登录
-	 * @method 发消息列表
-	 * @param {String} page/limit/order
-	 * @return {JSON}
-	 * 返回结果与 收消息列表(/api/messages/receivelist) 相似，只是'sender'变为'receiver'
-	 */
-	app.get('/api/messages/sendlist', function (req, res, next) {
-		var user = req.user;
-		if (!user) return next(errors(21301));
-		req.query['senderid'] = user.id;
-		Messages.forge().fetch({req: req, related: ['receiver'] })
-			.then(function (messages) {
-				next({ messages: messages});
-			}).catch(next);
-	});
-
+	
 	/**
 	 * GET /api/messages/unreadcount <br/>
 	 * 需登录
@@ -127,68 +61,6 @@ module.exports = function (app) {
 	});
 
 	/**
-	 * POST /api/messages/reply
-	 * @method 回复消息
-	 * @param {Number} sourceid 要回复的消息id
-	 * @param {String} title 标题
-	 * @param {String} body 内容
-	 * @return {JSON}
-	 * {
-			msg: 'message replied',
-			id: 1
-		}
-	 */
-	app.post('/api/messages/reply', function (req, res, next) {
-		if (!req.user) return next(errors(21301));
-		req.query['senderid'] = req.user.id;
-		Message.forge({id: req.query['sourceid']})
-			.fetch()
-			.then(function (sourceMessage) {
-				if (!sourceMessage) return next(errors(20603));
-				req.query['receiverid'] = sourceMessage.get('senderid');
-				return Message.forge(req.query)
-					.send()
-					.then(function (message) {
-						next({
-							msg: 'message replied',
-							id: message.id
-						});
-						sourceMessage.set({isreplied: 1}).save();
-					});
-			}).catch(next);
-
-	});
-
-	/**
-	 * POST /api/messages/markread
-	 * @method 标记为已读
-	 * @param {Number} id 消息id
-	 * @return {JSON}
-	 * {
-			msg: 'message markread success',
-			id: 1
-		}
-	 */
-	app.post('/api/messages/markread', function (req, res, next) {
-		if (!req.user) return next(errors(21301));
-		if (!req.query['id']) return next(errors(10008));
-		Message.forge(_.pick(req.query, 'id'))
-			.fetch()
-			.then(function (message) {
-				if (message.get('receiverid') != req.user.id)
-					return next(errors(20102));
-				return message.set({isread: 1})
-					.save()
-					.then(function (m) {
-						next({
-							msg: 'message markread success',
-							id: m.id
-						});
-					});
-			}).catch(next);
-	});
-
-	/**
 	 * GET /api/messages/unreadlist <br>
 	 * 需登录，不分页
 	 * @method 未读消息列表
@@ -229,7 +101,6 @@ module.exports = function (app) {
 		]
 	}
 	 </pre>
-	 *
 	 */
 	app.get('/api/messages/unreadlist', function (req, res, next) {
 		if (!req.user) return next(errors(21301));
@@ -296,7 +167,6 @@ module.exports = function (app) {
 		]
 		}
 	 </pre>
-	 *
 	 */
 	app.get('/api/messages/record', function (req, res, next) {
 		if (!req.user) return next(errors(21301));
